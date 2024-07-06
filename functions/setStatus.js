1a77e3d4dc5adad23ca7d030475c0420cf99646c
SreeOp <134767890+SreeOp@users.noreply.github.com>
2024-07-06 20:33:55 +0530

    setStatus.js

--- functions/setStatus.js
+++ functions/setStatus.js
@@ -1,7 +1,29 @@
-// functions/setStatus.js
+const { ActivityType } = require('discord.js');
+
+// Array of status messages
+const statusMessages = [
+  "No Skill Customs",
+  "Vehicle Works",
+  "Maps Works",
+  "Cinematics Works"
+];
+
+let currentIndex = 0;
+
 module.exports = (client) => {
-  client.user.setPresence({
-    activities: [{ name: 'ZyX', type: 'PLAYING' }],
-    status: 'online',
-  });
+  function updateStatus() {
+    const currentStatus = statusMessages[currentIndex];
+    currentIndex = (currentIndex + 1) % statusMessages.length;
+
+    client.user.setPresence({
+      activities: [{ name: currentStatus, type: ActivityType.Custom }],
+      status: 'dnd',
+    });
+  }
+
+  // Update status every 10 minutes (600000 ms)
+  setInterval(updateStatus, 600000);
+
+  // Initial status set
+  updateStatus();
 };
