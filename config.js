a0f5b748ef32cc528b6a5687ee400cc2ad237c7f
SreeOp <134767890+SreeOp@users.noreply.github.com>
2024-07-07 21:28:40 +0530

    config.js

--- config.js
+++ config.js
@@ -1,9 +1,5 @@
-// config.js
 module.exports = {
-  allowedRoles: [
-    '1007930481716760666',
-    '1046786167644880946',
-    'ROLE_ID_3',
-    'ROLE_ID_4',
-  ],
+  DISCORD_TOKEN: process.env.DISCORD_TOKEN,
+  allowedRoles: ['1007930481716760666', '1046786167644880946', 'roleID3', 'roleID4'], // Replace with your allowed roles IDs
+  autorole: '1007669179979157574' // Replace with the ID of the autorole
 };
