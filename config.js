34b0688eb282c8798413a02c1253201a458fd456
SreeOp <134767890+SreeOp@users.noreply.github.com>
2024-07-24 21:28:25 +0530

    config.js

--- config.js
+++ config.js
@@ -1,8 +1,12 @@
 // config.js
 module.exports = {
-  allowedRoles: [
-    '1007930481716760666',
-    '1046786167644880946',
-    'ROLE_ID_3',
-    'ROLE_ID_4',
-  ],
+  allowedRoles: ['1007930481716760666', '1046786167644880946'], // Example role IDs
+  applicationChannelId: '1255162116126539786', // Default value
+  questions: {
+    apply: [
+      'What Is Your Name?',
+      'Age',
+      'Experience'
+    ]
+  }
+};
