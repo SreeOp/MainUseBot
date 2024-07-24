c29deb91c1d07c563e1595ab91da559bf6cf9e69
SreeOp <134767890+SreeOp@users.noreply.github.com>
2024-07-24 19:43:30 +0530

    config.js

--- config.js
+++ config.js
@@ -6,4 +6,19 @@ module.exports = {
     'ROLE_ID_3',
     'ROLE_ID_4',
   ],
+  channelId: '1255162116126539786', // Channel ID where applications are reviewed
+  questions: {
+    'Staff': [
+      'Why do you want to join the staff team?',
+      'What experience do you have with moderation?',
+    ],
+    'Vehicle Developer': [
+      'What skills do you have in vehicle development?',
+      'Can you provide examples of your previous work?',
+    ],
+    'Developer': [
+      'What programming languages are you proficient in?',
+      'Can you describe a project you have worked on?',
+    ],
+  }
 };
