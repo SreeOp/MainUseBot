0d1177cdeb0d03e8c6126d1267e163b8d81df9ac
SreeOp <134767890+SreeOp@users.noreply.github.com>
2024-07-24 20:08:38 +0530

    setapplication.js

--- commands/setapplication.js
+++ commands/setapplication.js
@@ -1,39 +1,37 @@
-const { SlashCommandBuilder } = require('discord.js');
-const { MessageActionRow, MessageButton } = require('discord.js');
+const { SlashCommandBuilder, MessageActionRow, MessageButton } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('setapplication')
-    .setDescription('Set up the application menu in a specified channel')
+    .setDescription('Sets up the application process.')
     .addChannelOption(option =>
       option.setName('channel')
-        .setDescription('Channel to set up the application menu')
+        .setDescription('Channel to send the application form')
         .setRequired(true)),
   async execute(interaction) {
     const channel = interaction.options.getChannel('channel');
 
-    // Create the application menu
     const row = new MessageActionRow()
       .addComponents(
         new MessageButton()
-          .setCustomId('apply_staff')
-          .setLabel('Apply for Staff')
+          .setCustomId('staff')
+          .setLabel('Staff Application')
           .setStyle('PRIMARY'),
         new MessageButton()
-          .setCustomId('apply_vehicle')
-          .setLabel('Apply for Vehicle Developer')
+          .setCustomId('vehicle')
+          .setLabel('Vehicle Application')
           .setStyle('PRIMARY'),
         new MessageButton()
-          .setCustomId('apply_developer')
-          .setLabel('Apply for Developer')
+          .setCustomId('developer')
+          .setLabel('Developer Application')
           .setStyle('PRIMARY')
       );
 
     await channel.send({
-      content: 'Click the button below to apply for a role:',
-      components: [row],
+      content: 'Click a button to start your application:',
+      components: [row]
     });
 
-    await interaction.reply({ content: 'Application menu set up successfully!', ephemeral: true });
+    await interaction.reply({ content: 'Application setup complete!', ephemeral: true });
   },
 };
