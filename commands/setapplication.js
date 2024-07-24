ea6ac72a070ea63842f7f64ec340409073957ea4
SreeOp <134767890+SreeOp@users.noreply.github.com>
2024-07-24 18:29:12 +0530

    Update setapplication.js

--- commands/setapplication.js
+++ commands/setapplication.js
@@ -4,22 +4,24 @@ const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('setapplication')
-    .setDescription('Set up the job application buttons.'),
+    .setDescription('Set up the application menu in the current channel'),
   async execute(interaction) {
     const row = new ActionRowBuilder().addComponents(
       new ButtonBuilder()
-        .setCustomId('apply_ems')
+        .setCustomId('ems')
         .setLabel('EMS')
         .setStyle(ButtonStyle.Primary),
       new ButtonBuilder()
-        .setCustomId('apply_pd')
+        .setCustomId('pd')
         .setLabel('PD')
         .setStyle(ButtonStyle.Primary)
     );
 
-    await interaction.reply({
-      content: 'Click the button below to apply for a staff position.',
-      components: [row],
+    await interaction.channel.send({
+      content: 'Select Job Application:',
+      components: [row]
     });
-  },
+
+    await interaction.reply({ content: 'Application menu has been set up!', ephemeral: true });
+  }
 };
