578dfbeb27c4480dae16eb33121af743994a8f10
SreeOp <134767890+SreeOp@users.noreply.github.com>
2024-07-31 10:19:09 +0530

    stay.js

--- commands/stay.js
+++ commands/stay.js
@@ -11,18 +11,27 @@ module.exports = {
         .setRequired(true)),
 
   async execute(interaction) {
-    const channel = interaction.options.getChannel('channel');
+    try {
+      const channel = interaction.options.getChannel('channel');
 
-    if (channel.type !== ChannelType.GuildVoice) {
-      return interaction.reply({ content: 'Please select a valid voice channel.', ephemeral: true });
+      if (channel.type !== ChannelType.GuildVoice) {
+        return await interaction.reply({ content: 'Please select a valid voice channel.', ephemeral: true });
+      }
+
+      const connection = joinVoiceChannel({
+        channelId: channel.id,
+        guildId: channel.guild.id,
+        adapterCreator: channel.guild.voiceAdapterCreator,
+      });
+
+      await interaction.reply({ content: `Joined ${channel.name} and staying there 24/7!`, ephemeral: true });
+    } catch (error) {
+      console.error(error);
+      if (!interaction.replied) {
+        await interaction.reply({ content: 'There was an error executing that command!', ephemeral: true });
+      } else {
+        await interaction.followUp({ content: 'There was an error executing that command!', ephemeral: true });
+      }
     }
-
-    const connection = joinVoiceChannel({
-      channelId: channel.id,
-      guildId: channel.guild.id,
-      adapterCreator: channel.guild.voiceAdapterCreator,
-    });
-
-    return interaction.reply({ content: `Joined ${channel.name} and staying there 24/7!`, ephemeral: true });
   },
 };
