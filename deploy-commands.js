6ca69f5465b1f16d9a38edf7fea0de37079f040b
SreeOp <134767890+SreeOp@users.noreply.github.com>
2024-08-10 19:39:54 +0530

    deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,6 +1,6 @@
 const { REST, Routes } = require('discord.js');
-const fs = require('fs');
 require('dotenv').config();
+const fs = require('fs');
 
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -12,17 +12,24 @@ for (const file of commandFiles) {
 
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
-(async () => {
+module.exports = async () => {
+  const clientId = process.env.CLIENT_ID;
+  const guildId = process.env.GUILD_ID;
+
+  if (!clientId || !guildId) {
+    throw new Error('CLIENT_ID or GUILD_ID is not defined in the environment variables');
+  }
+
   try {
     console.log('Started refreshing application (/) commands.');
 
     await rest.put(
-      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
-      { body: commands }
+      Routes.applicationGuildCommands(clientId, guildId),
+      { body: commands },
     );
 
     console.log('Successfully reloaded application (/) commands.');
   } catch (error) {
     console.error(error);
   }
-})();
+};
