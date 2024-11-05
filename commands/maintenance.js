4ea05794973bf1a59cd6cbdfaf15df55a3517a2e
SreeOp <134767890+SreeOp@users.noreply.github.com>
2024-11-05 15:53:43 +0530

    Update maintenance.js

--- commands/maintenance.js
+++ commands/maintenance.js
@@ -3,8 +3,8 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('maintenance')
-    .setDescription('Set server status to Online or Under Maintenance')
-    .addStringOption(option =>
+    .setDescription('Set server status to Online or Under Maintenance with a direct message to a channel')
+    .addStringOption(option => 
       option.setName('status')
         .setDescription('Select the server status')
         .setRequired(true)
@@ -13,21 +13,15 @@ module.exports = {
           { name: 'Server Under Maintenance', value: 'maintenance' }
         )
     ),
-
+  
   async execute(interaction) {
-    const channelId = '1255162116126539786'; // Replace with the ID of the channel you want to send the message to
-    const roleId = '1046786167644880946'; // Replace with your role ID
+    const channelId = '1266779806582702171'; // Replace with your target channel ID
+    const roleId = '1299270070534668324'; // Replace with your role ID
     const onlineGif = 'https://cdn.discordapp.com/attachments/1056903195961610275/1242682120141275176/standard_3.gif?ex=672ae3e9&is=67299269&hm=2ef583f9da3738c604d582db5d86f145815797880382c931b4b94f39f84d9522&'; // Replace with the URL of an animated GIF for online
     const maintenanceGif = 'https://cdn.discordapp.com/attachments/1056903195961610275/1242682120141275176/standard_3.gif?ex=672ae3e9&is=67299269&hm=2ef583f9da3738c604d582db5d86f145815797880382c931b4b94f39f84d9522&'; // Replace with the URL of an animated GIF for maintenance
 
     // Retrieve the selected server status
     const status = interaction.options.getString('status');
-    const channel = await interaction.client.channels.fetch(channelId);
-
-    if (!channel) {
-      console.error(`Channel with ID ${channelId} not found.`);
-      return;
-    }
 
     // Embed setup based on status
     const embed = new EmbedBuilder()
@@ -45,8 +39,17 @@ module.exports = {
         .setImage(maintenanceGif);
     }
 
-    // Send the embed to the specified channel
-    await channel.send({ embeds: [embed] });
-    await interaction.reply({ content: 'Message sent to the channel.', ephemeral: true });
+    try {
+      // Fetch the channel and send the message there
+      const channel = await interaction.client.channels.fetch(channelId);
+      if (channel) {
+        await channel.send({ embeds: [embed] });
+        await interaction.deferReply({ ephemeral: true });
+      } else {
+        console.error('Channel not found');
+      }
+    } catch (error) {
+      console.error('Error sending the maintenance message:', error);
+    }
   },
 };
