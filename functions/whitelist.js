40e97dfafb36501d1e557f4f1b2ef2a639a18c0a
SreeOp <134767890+SreeOp@users.noreply.github.com>
2025-01-18 16:33:03 +0530

    whitelist.js

--- functions/whitelist.js
+++ functions/whitelist.js
@@ -111,7 +111,7 @@ module.exports = (client) => {
       }
 
       async function generateTicketImage(details, imageURL) {
-        const canvas = createCanvas(800, 400);
+        const canvas = createCanvas(1024, 331);
         const ctx = canvas.getContext('2d');
         const background = await loadImage(imageURL);
 
